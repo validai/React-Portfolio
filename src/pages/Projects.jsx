58392823666297696d22c564b8b1c220d06c92f3
validai <djcoast239@gmail.com>
2025-02-24 17:15:38 -0500

    Fixed type in preojects file

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => (
     <p>Here’s a collection of my latest work:</p>
     <ul>
       <li><strong>Flare48</strong> - A custom news aggregator focusing on the latest 48-hour news updates.</li>
-      <li><strong>SkillStarter</strong> - A web platform allowing users to create profiles and showcase portfolio projects. Features include secure authentication, Cloudinary image uploads, and a responsive design optimized for all devices.</li>
+      <li><strong>SkillStarter</strong> - A web platform allowing users to create profiles and showcase portfolio projects. </li>
       <li><strong>Task Manager App</strong> - A productivity tool to track and organize tasks effectively.</li>
     </ul>
   </div>
