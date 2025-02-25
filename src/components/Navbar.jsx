10c422b47969f0e997a4628e7e4ace00314b21a4
validai <djcoast239@gmail.com>
2025-02-24 19:31:56 -0500

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/global.css";
+import background from "src/assets/IMG_1472.jpg";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <header className="header" style={{ backgroundImage: "url('/assets/IMG_1472.jpg')" }}>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/resume">Resume</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 export default Navbar;
