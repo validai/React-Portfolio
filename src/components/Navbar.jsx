bbab3d506652b23157d5789788db551923cc7ef4
validai <djcoast239@gmail.com>
2025-02-26 18:03:35 -0500

    Added name to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Assuming you have a CSS file for styling
+import "..sytyles/global.css"; // Ensure this file exists
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li><Link to="/" className="nav-item">Eric Cordoba</Link></li>
+        <li className="nav-item name">Eric Cordoba</li>
         <li><Link to="/about" className="nav-item">About Me</Link></li>
         <li><Link to="/portfolio" className="nav-item">Portfolio</Link></li>
         <li><Link to="/resume" className="nav-item">Resume</Link></li>
