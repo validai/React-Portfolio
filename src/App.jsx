3b3a3bb0053825796becd4c90e05070d7c2fc133
validai <djcoast239@gmail.com>
2025-02-26 20:09:00 -0500

    changed name of page

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portofolio />} />
+            <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
