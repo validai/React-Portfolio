88debe00363183401a7300f0416338b15b921639
validai <djcoast239@gmail.com>
2025-02-26 20:27:17 -0500

    fixed css for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 import "../styles/global.css"; // Ensure this file exists
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li className="nav-item name">Eric Cordoba</li>
-        <li><Link to="/about" className="nav-item">About Me</Link></li>
-        <li><Link to="/portfolio" className="nav-item">Portfolio</Link></li>
-        <li><Link to="/resume" className="nav-item">Resume</Link></li>
-        <li><Link to="/contact" className="nav-item">Contact</Link></li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <span className="navbar-name">Eric Cordoba</span>
+            <ul className="nav-links">
+                <li><Link to="/about" className="nav-item">About Me</Link></li>
+                <li><Link to="/portfolio" className="nav-item">Portfolio</Link></li>
+                <li><Link to="/resume" className="nav-item">Resume</Link></li>
+                <li><Link to="/contact" className="nav-item">Contact</Link></li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
